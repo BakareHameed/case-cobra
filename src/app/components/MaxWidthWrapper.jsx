6eb7448d04e03c905785d9a17939478f39b077ce
BakareHameed <bakrehameed07@gmall.com>
2024-08-07 16:05:33 +0100

    initial commit

---
+++ src/app/components/MaxWidthWrapper.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+const MaxWidthWrapper = () => {
+  return (
+    <div className="h-full mx-auto w-full max-w-screen-xl">MaxWidthWrapper</div>
+  )
+}
+
+export default MaxWidthWrapper
---
+++ src/app/page.js
@@ -0,0 +1,10 @@
+import Image from "next/image";
+import MaxWidthWrapper from "./components/MaxWidthWrapper";
+
+export default function Home() {
+  return (
+    <div className="bg-slate-50">
+      <MaxWidthWrapper/>
+    </div>
+  );
+}
